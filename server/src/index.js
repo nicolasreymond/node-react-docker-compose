ec5b00bc8af98dccdf30ff7f6421750a1a05e55b
Nicolas Reymond <n.reymond.12@gmail.com>
2020-06-24 11:02:00 +0200

    Forked repo

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, I\'m nreymond!!!!'
   };
   res.send(JSON.stringify(data, null, 2));
 });
